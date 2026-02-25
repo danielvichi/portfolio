58ffed39296fd945eaa6d654b70ddfcc08aa03ef
danielvichi <danielvichi@gmail.com>
2026-02-25 15:03:22 -0300

    Improve project-card backgrounds

--- src/app/_modules/page-sections/previous-experience-role-section/project-card.tsx
+++ src/app/_modules/page-sections/previous-experience-role-section/project-card.tsx
@@ -1,4 +1,5 @@
 import Markdown from "react-markdown";
+import ChevronRight from "~/app/_icons/chevron-right";
 
 export interface ProjectCardProps {
   title: string;
@@ -7,17 +8,56 @@ export interface ProjectCardProps {
   coverImageUrl?: string;
 }
 
+function ImageBackground({ imgUrl }: { imgUrl: string }) {
+  return (
+    <div
+      className="absolute inset-[2px] brightness-50 transition-all group-hover:scale-110"
+      style={{
+        backgroundImage: `url(${imgUrl})`,
+        backgroundSize: "cover",
+        backgroundOrigin: "center",
+        backdropFilter: "blur(2px)",
+      }}
+    />
+  );
+}
+
+function DevIcon() {
+  return (
+    <>
+      <ChevronRight className="stroke-accent-tertiary h-2/3 rotate-180 fill-none stroke-3" />
+      <div className="border-accent-tertiary mx-5 h-[110px] w-[15px] rotate-30 border-[1.5px]" />
+      <ChevronRight className="stroke-accent-tertiary h-2/3 fill-none stroke-3" />
+    </>
+  );
+}
+
+function DevBackground() {
+  return (
+    <div className="absolute inset-[2px] flex items-center justify-end brightness-50 transition-all group-hover:scale-110">
+      <DevIcon />
+    </div>
+  );
+}
+
 export default function ProjectCard(props: ProjectCardProps) {
   const { title, description, coverImageUrl, projectUrl } = props;
   return (
-    <div className="relative flex max-h-[70vh] w-full min-w-[300px] flex-col gap-4 overflow-y-scroll border border-gray-500 p-4">
-      <h5 className="bg-accent-tertiary relative w-fit px-2 py-0.5">{title}</h5>
-      <Markdown>{description}</Markdown>
-      {coverImageUrl ? (
-        <img src={coverImageUrl} alt={`${title} project sample image`} />
-      ) : (
-        ""
-      )}
+    <div className="bg-background-secondary relative flex max-h-[70vh] w-full min-w-[300px] flex-col gap-4 overflow-y-scroll border border-gray-500 p-4">
+      <div className="absolute top-0 right-0 h-[150px] w-[300px] opacity-50">
+        <DevBackground />
+      </div>
+      <h5 className="bg-accent-tertiary relative w-fit px-2 py-0.5 text-2xl">
+        {title}
+      </h5>
+      <div className="flex flex-col p-4 px-8 md:px-16">
+        <Markdown>{description}</Markdown>
+        {coverImageUrl ? (
+          <img src={coverImageUrl} alt={`${title} project sample image`} />
+        ) : (
+          ""
+        )}
+      </div>
     </div>
   );
 }
@@ -26,15 +66,11 @@ function ProjectCardThumbnail(props: ProjectCardProps) {
   const { title, coverImageUrl } = props;
   return (
     <div className="group hover:border-accent-primary relative h-[150px] w-[400px] overflow-hidden border border-gray-500 p-4 transition-all">
-      <div
-        className="absolute inset-[2px] brightness-50 transition-all group-hover:scale-110"
-        style={{
-          backgroundImage: `url(${coverImageUrl})`,
-          backgroundSize: "cover",
-          backgroundOrigin: "center",
-          backdropFilter: "blur(2px)",
-        }}
-      />
+      {coverImageUrl ? (
+        <ImageBackground imgUrl={coverImageUrl} />
+      ) : (
+        <DevBackground />
+      )}
       <h5 className="bg-accent-tertiary relative w-fit px-2 py-0.5">{title}</h5>
     </div>
   );
