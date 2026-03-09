bd049fa7e424f51c3bee87e813614a16b6249262
danielvichi <danielvichi@gmail.com>
2026-03-09 17:57:19 -0300

    Add hero intro animation

--- src/store/useInteractionsStore.ts
+++ src/store/useInteractionsStore.ts
@@ -2,14 +2,18 @@ import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
 export interface InteractionsState {
+  isFirstRender: boolean;
   logoInteracted: boolean;
+  setIsFirstRender: (isFirstRender: boolean) => void;
   setLogoInteracted: (interacted: boolean) => void;
 }
 
 const useInteractionsStore = create<InteractionsState>()(
   persist(
     (set) => ({
+      isFirstRender: true,
       logoInteracted: false,
+      setIsFirstRender: (isFirstRender: boolean) => set({ isFirstRender }),
       setLogoInteracted: (interacted: boolean) =>
         set({ logoInteracted: interacted }),
     }),
